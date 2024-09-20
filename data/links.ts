3d4482d669a04b9cfe0b1949e1f15b20250a1afa
dnyaneshwarshekade <shekade31@gmail.com>
2024-09-20 22:08:09 +0530

    update

--- data/links.ts
+++ data/links.ts
@@ -1,10 +1,10 @@
 const links = {
   instagram: "https://instagram.com/shekade_dnyaneshwar", // Update with your Instagram link
   linkedin: "https://linkedin.com/in/dnyaneshwarshekade", // Update with your LinkedIn link
-  twitter: "https://yourwebsite.com/twitter", // Update with your Twitter link
+  twitter: "https://x.com/shekade31", // Update with your Twitter link
   github: "https://github.com/dnyaneshwarshekade", // Your GitHub link
   dribbble: "https://yourwebsite.com/dribbble", // Update with your Dribbble link
-  dev: "https://yourwebsite.com/dev", // Update with your Dev link
+  dev: "https://dev.to/dnyaneshwarshekade", // Update with your Dev link
   facebook: "https://yourwebsite.com/facebook", // Update with your Facebook link
   resume: "https://yourwebsite.com/resume-pdf", // Update with your resume link
   repository: "https://github.com/dnyaneshwarshekade/portfolio.git", // Your portfolio repository link
