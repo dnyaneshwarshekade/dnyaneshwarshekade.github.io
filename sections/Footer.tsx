77b4dd7e847e18a31f81c394f1ecd4bdbb1b5ff4
dnyaneshwarshekade <shekade31@gmail.com>
2024-09-21 06:48:28 +0530

    update

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,11 +1,50 @@
 import { format } from "date-fns";
+import { useState } from "react";
 
-const Footer = () => (
-  <div id="footer" className="mb-16 text-xs leading-loose opacity-30">
-    Licensed under MIT.
-    <br />
-    Copyright {format(Date.now(), "yyyy")} Dnyaneshwar Shekade.
-  </div>
-);
+const Footer = () => {
+  const [loading, setLoading] = useState(false);
+  const [loadCompleted, setLoadCompleted] = useState(false);
+
+  const handleLoadClick = () => {
+    if (loadCompleted) {
+      window.location.href = "https://dnyaneshwarshekade.github.io";
+    } else {
+      setLoading(true);
+      // Simulate loading process
+      setTimeout(() => {
+        setLoading(false);
+        setLoadCompleted(true);
+      }, 2000); // Adjust time as needed
+    }
+  };
+
+  const handleMouseEnter = () => {
+    // Optional: handle mouse enter events if needed
+  };
+
+  return (
+    <div id="footer" className="mb-16 text-xs leading-loose opacity-30">
+      <div
+        className="loading-bar-container"
+        onClick={handleLoadClick}
+        onMouseEnter={handleMouseEnter}
+      >
+        <div className={`loading-bar ${loadCompleted ? 'loaded' : loading ? 'loading' : ''}`}>
+          <span>
+            {loadCompleted
+              ? "Loaded Ubuntu Portfolio Click to check"
+              : loading
+              ? "Loading Ubuntu..."
+              : "Click here to Load Dnyaneshwar Ubuntu Portfolio"}
+          </span>
+        </div>
+      </div>
+      
+      Licensed under MIT.
+      <br />
+      Copyright {format(Date.now(), "yyyy")} Dnyaneshwar Shekade.
+    </div>
+  );
+};
 
 export default Footer;
--- sections/Music.tsx
+++ sections/Music.tsx
@@ -1,45 +1,55 @@
+import React, { useState } from "react";
 import Button from "components/Button";
 import { FaSpotify } from "react-icons/fa";
 import { Section } from "types/Sections";
-import { getSectionHeading, openURLInNewTab } from "utils";
+import { getSectionHeading } from "utils";
 
-const Music = () => (
-  <div id={Section.Music}>
-    {getSectionHeading(Section.Music)}
+const Music = () => {
+  const [isIframeVisible, setIframeVisible] = useState(false);
 
-    <div className="grid md:grid-cols-3 gap-12">
-      <div className="max-w-full prose prose-sm md:prose-base prose-neutral dark:prose-invert">
-        <p>
-          If you&apos;ve read about my profile so far and you found it interesting, studies show that knowing what type
-          of music a person listens to would say a lot about them.
-        </p>
+  const handleButtonClick = () => {
+    setIframeVisible(true);
+  };
 
-        <p>
-          Also, I&apos;ve been known for my good taste in music and I&apos;ve always wanted to share the latest and
-          greatest hits through an easy medium, so here&apos;s a Spotify Playlist I&apos;ve created called My Everyday:
-          A dynamic playlist of what&apos;s on my daily roster of music.
-        </p>
+  return (
+    <div id={Section.Music}>
+      {getSectionHeading(Section.Music)}
 
-        <Button
-          icon={FaSpotify}
-          className="mt-8 text-sm"
-          onClick={() => openURLInNewTab("https://open.spotify.com/embed/playlist/4ynog50nYPDm024X0NJ9Ho?utm_source=generator")}
-        >
-          Listen to My Everyday on Spotify
-        </Button>
+      <div className="grid md:grid-cols-3 gap-12">
+        <div className="max-w-full prose prose-sm md:prose-base prose-neutral dark:prose-invert">
+          <p>
+            If you&apos;ve read about my profile so far and you found it interesting, studies show that knowing what type
+            of music a person listens to would say a lot about them.
+          </p>
+
+          <p>
+            Also, I&apos;ve been known for my good taste in music and I&apos;ve always wanted to share the latest and
+            greatest hits through an easy medium, so here&apos;s a Spotify Playlist I&apos;ve created called My Everyday:
+            A dynamic playlist of what&apos;s on my daily roster of music.
+          </p>
+
+          <Button
+            icon={FaSpotify}
+            className="mt-8 text-sm"
+            onClick={handleButtonClick}
+          >
+            Listen to My Everyday on Spotify
+          </Button>
+        </div>
+
+        {isIframeVisible && (
+          <iframe
+            src="https://open.spotify.com/embed/playlist/4ynog50nYPDm024X0NJ9Ho?utm_source=generator"
+            width="100%"
+            height="512"
+            frameBorder="0"
+            allow="encrypted-media"
+            className="md:col-span-2 rounded col-span-full"
+          />
+        )}
       </div>
-
-      <iframe
-        src="https://open.spotify.com/embed/playlist/4ynog50nYPDm024X0NJ9Ho?utm_source=generator"
-        width="100%"
-        height="512"
-        frameBorder="0"
-        allow="encrypted-media"
-        className="hidden md:block rounded col-span-2"
-      />
     </div>
-  </div>
-);
+  );
+};
 
 export default Music;
-
