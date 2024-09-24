91347b137bc57cf9e3199276cbd0cb5759c483b4
dnyaneshwarshekade <shekade31@gmail.com>
2024-09-24 15:30:37 +0530

    remove some unecessory things

--- data/achievements.ts
+++ data/achievements.ts
@@ -3,28 +3,8 @@ import type { Achievement } from "types/Sections";
 const achievementList: Achievement[] = [
   {
     id: 1,
-    title: "International Flutter Hackathon '19",
-    subtitle: "1st Place",
-  },
-  {
-    id: 2,
-    title: "Venturesity Banyan Hack '16",
-    subtitle: "1st Place",
-  },
-  {
-    id: 3,
-    title: "eGovernments Foundation Hackathon '17",
-    subtitle: "1st Place",
-  },
-  {
-    id: 4,
-    title: "Smart India Hackathon '17",
-    subtitle: "1st Place",
-  },
-  {
-    id: 5,
-    title: "NIT Trichy - Pragyan Hackathon '16",
-    subtitle: "1st Place",
+    title: "TechFest In N.G Acharya '19",
+    subtitle: "Participant",
   },
 ];
 
